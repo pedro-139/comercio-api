f98187f738a3e88a1ca89c69c6b237359962e430
Pedro Serrano <pedroserrano212@gmail.com>
2026-09-11 11:59:35 -0300

    test: corregir y completar tests unitarios

--- src/test/java/com/bricks/productos_api/service/impl/ProductoServiceImplTest.java
+++ src/test/java/com/bricks/productos_api/service/impl/ProductoServiceImplTest.java
@@ -30,7 +30,7 @@ class ProductoServiceImplTest {
     private ProductoRepository productoRepository;
 
     @Mock
-    private CategoriaService categoriaService;
+    private CategoriaServiceImpl categoriaService;
 
     @Mock
     private ProductoMapper productoMapper;
